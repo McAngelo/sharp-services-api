e8c2477800f8bb40270a08c1fb802f36e0fefb00
Michael Johnson <mcangelo200@gmail.com>
2020-02-20 21:55:22 +0000

    --added the authentication middleware to the project

---
+++ _middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+
+const auth = async (req, res, next) => {
+	//console.log('auth middleware', req.header);
+
+	try {
+		const token = req.header('Authorization').replace('Bearer ', '');
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+		//console.log(token);
+
+		if(!user){
+			throw new Error();
+		}
+		
+		req.token = token;
+		req.user = user;
+		next();
+
+	} catch(err) {
+		// statements
+		//console.log(err);
+		res.status(401).send({error: 'Please authenticate.'})
+	}
+
+	//next();
+}
+
+module.exports = auth;
